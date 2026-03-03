83f822cdec98a4d86438b14e0999b53cec2c0538
Aatmaj <Aatmaj.mhatre@gmail.com>
2026-03-03 12:45:42 +0530

    Added torch debugging

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 """Training script for ML model."""
 import argparse
+import torch
 
-from helpers.config import config,load_config
-from helpers.logger import log,setup_logger
+from helpers.config import config, load_config
+from helpers.logger import log, setup_logger
 from helpers.telemetry_writer import telemetry_writer
 
 from trainer import train
@@ -13,7 +14,6 @@ parser.add_argument("--mode", help="Run mode (debug/prod)",
 args = parser.parse_args()
 load_config(args.mode)
 
-debug = args.mode == "debug"
 
 setup_logger()
 telemetry_writer.setup_writer(
@@ -22,5 +22,12 @@ telemetry_writer.setup_writer(
 
 
 if __name__ == "__main__":
-    log.info(f"Starting training with run_id: <red>{config.runtime.RUN_ID}</red> at time: {config.runtime.START_TIME}")
+    if config.IS_DEBUG_TORCH:
+        # Note: If using DDP with torch.multiprocessing.spawn, anomaly detection must
+        # be enabled inside each worker process, not only in the main file.
+        torch.autograd.set_detect_anomaly(True)
+        torch.set_warn_always(True)
+
+    log.info(
+        f"Starting training with run_id: <red>{config.runtime.RUN_ID}</red> at time: {config.runtime.START_TIME}")
     train()
