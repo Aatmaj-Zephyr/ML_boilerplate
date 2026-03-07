a04b4d3de71bccc99a5298085a832c1579b9070a
Aatmaj <Aatmaj.mhatre@gmail.com>
2026-03-07 19:20:41 +0530

    Added multiprocessing safe main file

--- main.py
+++ main.py
@@ -8,20 +8,20 @@ from helpers.telemetry_writer import telemetry_writer
 
 from trainer import train
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--mode", help="Run mode (debug/prod)",
-                    choices=["debug", "prod"], default="debug")
-args = parser.parse_args()
-load_config(args.mode)
-
-
-setup_logger()
-telemetry_writer.setup_writer(
-    fieldnames=["epoch", "train_loss", "val_loss"]  # modify this as needed
-)
-
-
 if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--mode", help="Run mode (debug/prod)",
+                        choices=["debug", "prod"], default="debug")
+    args = parser.parse_args()
+    load_config(args.mode)
+
+
+    setup_logger()
+    telemetry_writer.setup_writer(
+        fieldnames=["epoch", "train_loss", "val_loss"]  # modify this as needed
+    )
+
     if config.IS_DEBUG_TORCH:
         # Note: If using DDP with torch.multiprocessing.spawn, anomaly detection must
         # be enabled inside each worker process, not only in the main file.
